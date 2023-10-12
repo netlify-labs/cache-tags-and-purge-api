7df1425594df4f46ea3b8f552118924ea2119c9c
Eduardo Bouças <mail@eduardoboucas.com>
2023-10-12 13:43:23 +0100

    update heading

--- netlify/functions/netlify-cache-tag.ts
+++ netlify/functions/netlify-cache-tag.ts
@@ -7,11 +7,11 @@ export default async () => {
   const json = await resp.json();
   const body = `<!doctype html><html>
     <head>
-        <title>Cache-Tag</title>
+        <title>Netlify-Cache-Tag</title>
         <link rel="stylesheet" href="/main.css">
     </head>
     <body>
-        <h1>Cache-Tag</h1>
+        <h1>Netlify-Cache-Tag</h1>
         <p>Similarly to the <a href="/cache-tag">Cache-Tag</a> header, the <strong>Netlify-Cache-Tag</strong> header allows you to tag content across Netlify's CDN so you can invalidate it more granularly. While other services you have in front of Netlify's CDN might support the Cache-Tag header, Netlify-Cache-Tag is only supported by Netlify, so you can have different cache tagging/purging behaviour for each service.
         <p>This page is tagged with <strong>"Netlify-Cache-Tag: jewelery,cache-tag-demo"</strong>, you can use the <strong>jewelery</strong> tag to purge only this page without affecting the rest of the content on this site, or use <strong>cache-tag-api-demo</strong> to purge all generated pages on this site</p>
         <h2>Products</h2>
